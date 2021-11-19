2e5f02b9f61e9e8cab34b44117d3d1b89123b902
muhamad-fadla <yuratamma@gmail.com>
2021-11-19 10:06:21 +0700

    initial commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+import Fastify from 'fastify';
+import FastifyStatic from 'fastify-static';
+import FastifyCors from 'fastify-cors';
+import FastifyCompress from 'fastify-compress';
+
+import * as path from 'path';
+
+const server = Fastify();
+
+
+server.register(FastifyCors, {
+	origin: [/\.sociality\.my.id$/, /\.my-topup\.store$/]
+});
+
+server.register(FastifyCompress, {
+	global: true,
+	encodings: ['deflate', 'gzip']
+});
+
+server.addHook('preHandler', (req, reply, done) => {
+	if(req.url.includes('immutable')){
+  		reply.header("Cache-Control", "public, max-age=604800, immutable");
+	}else{
+  		reply.header("Cache-Control", "public, max-age=604800");
+	}
+
+  	done();
+});
+
+
+server.get('/.hit', (req,res) => {
+	res.status(200).send('ok');
+})
+
+server.register(FastifyStatic, {
+	root: path.join(__dirname, './static')
+});
+
+
+server.listen(process.env.PORT || 8080/**, "0.0.0.0" */, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
